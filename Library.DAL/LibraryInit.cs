ecf5d5e049f8fe76f30297e6bcc7d884aa4402cb
Leysun <leysunny21@gmail.com>
2021-08-11 22:50:20 +0300

    delete dal

--- Library.DAL/LibraryInit.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data.Entity;
-
-namespace Library.DAL
-{
-    internal class LibraryInit : MigrateDatabaseToLatestVersion
-       <LibraryContext, Migrations.Configuration>
-    {
-    }
-}
